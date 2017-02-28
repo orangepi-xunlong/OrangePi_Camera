ab2871083c06988603c7ff0aff1ca9e15fe2204a
orangepi <buddy.d.zhang@gmail.com>
2017-02-28 17:03:39 +0800

    [PATCH] exprot header

--- include/OrangePiV4L2/OrangePiV4L2.h
+++ include/OrangePiV4L2/OrangePiV4L2.h
@@ -71,4 +71,6 @@ void OrangePi_Process_Image(struct OrangePi_v4l2_device *, const char *);
 /* Capture one picture */
 void OrangePi_device_captureOne(struct OrangePi_v4l2_device *, const char *);
 
+extern int write_JPEG_file(char *, unsigned char*, int, int, int);
+
 #endif
