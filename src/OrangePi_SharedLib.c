6583028328e0533bae6f88fa93ac5ada82ed3a73
orangepi <buddy.d.zhang@gmail.com>
2017-02-28 14:37:24 +0800

    [PATCH] v4l2: add basic v4l2 API

--- src/OrangePi_SharedLib.c
+++ src/OrangePi_SharedLib.c
@@ -1,5 +1,24 @@
 #include <OrangePiV4L2/OrangePiV4L2.h>
 
+/*
+ * Initialize a OrangePi Camera device
+ */
+struct OrangePi_v4l2_device *OrangePi_device_init(void)
+{
+    /* Prepare v4l2 device */
+    OrangePi.init(&OrangePi);
+    
+    return &OrangePi;    
+}
+
+/*
+ * Close a OrangePi Camera device
+ */
+void OrangePi_device_close(struct OrangePi_v4l2_device *dev)
+{
+    dev->close(dev);    
+}
+
 int add(int a, int b)
 {
     return a + b;    
--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    struct OrangePi_v4l2_device *dev;
+
+    dev = OrangePi_device_init();
+    OrangePi_device_close(dev);
+
     printf("1 + 1 = %d\n", add(1, 1));
     printf("2 + 1 = %d\n", sub(2, 1));
     printf("Hello World\n");  
